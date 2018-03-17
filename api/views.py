e2a6cd3f1d004a37e1449ce81beb0d303ec7da65
gootaa <37030485+gootaa@users.noreply.github.com>
2018-03-17 02:26:14 +0200

    Update views.py

--- api/views.py
+++ api/views.py
@@ -14,3 +14,17 @@ class RiverViewSet(APIView):
 
     def get(self, request, format=None):
         return JsonResponse(coreapi.Client().get('http://riverbrain.com/api/v1/rivers'), safe=False)
+
+def get_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        print("returning FORWARDED_FOR")
+        ip = x_forwarded_for.split(',')[-1].strip()
+    elif request.META.get('HTTP_X_REAL_IP'):
+        print("returning REAL_IP")
+        ip = request.META.get('HTTP_X_REAL_IP')
+    else:
+        print("returning REMOTE_ADDR")
+        ip = request.META.get('REMOTE_ADDR')
+    print(ip)
+    return HttpResponse(str(ip))
